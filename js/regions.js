c933d7641deaf9f711571ab710a4982d0c6a0590
Paulo Lima <paulo.lima@zrp.com.br>
2022-02-09 02:02:41 -0300

    chore: added insert validation with toast indicator

--- js/regions.js
+++ js/regions.js
@@ -1,3 +1,11 @@
+function showSaveWithSuccessToast() {
+  $("#saveWithSuccessToast").toast("show");
+};
+
+function showNotSaveWithSuccessToast() {
+  $("#notSaveWithSuccessToast").toast("show");
+};
+
 function saveRegionData(index) {
   const regionDataToSave = {
     [REGIONS_DATA[index].country]: [
@@ -6,22 +14,31 @@ function saveRegionData(index) {
   };
   const REGIONS_DATA_STORAGE = JSON.parse(localStorage.getItem("REGIONS_DATA_STORAGE"));
 
+  // Quando não existe nenhuma região salva
   if (!REGIONS_DATA_STORAGE) {
-    // Quando não existe nenhuma região salva
     localStorage.setItem("REGIONS_DATA_STORAGE", JSON.stringify(regionDataToSave));
+    showSaveWithSuccessToast();
   } else {
     const regionDataHasSaved = REGIONS_DATA_STORAGE[REGIONS_DATA[index].country];
 
+    // Quando já existe a região salva, um novo objeto é salvo na lista dessa região
     if (regionDataHasSaved) {
-      // Quando já existe a região salva, um novo objeto é salvo na lista dessa região
-      REGIONS_DATA_STORAGE[REGIONS_DATA[index].country].push(REGIONS_DATA[index]);
+      const hasRegionDataWithSameLastUpdateValue = REGIONS_DATA_STORAGE[REGIONS_DATA[index].country].filter((region) => region.lastUpdate === REGIONS_DATA[index].lastUpdate);
+
+      if (hasRegionDataWithSameLastUpdateValue) {
+        showNotSaveWithSuccessToast();
+      } else {
+        REGIONS_DATA_STORAGE[REGIONS_DATA[index].country].push(REGIONS_DATA[index]);
       
-      localStorage.setItem("REGIONS_DATA_STORAGE", JSON.stringify(REGIONS_DATA_STORAGE));
+        localStorage.setItem("REGIONS_DATA_STORAGE", JSON.stringify(REGIONS_DATA_STORAGE));
+        showSaveWithSuccessToast();
+      }
     } else {
       // Quando a região não existe mas já existe outra região, a nova região é concatenada as demais regiões
       const NEW_REGIONS_DATA_STORAGE = Object.assign(REGIONS_DATA_STORAGE, regionDataToSave);
 
       localStorage.setItem("REGIONS_DATA_STORAGE", JSON.stringify(NEW_REGIONS_DATA_STORAGE));
+      showSaveWithSuccessToast();
     }
   }
 };
